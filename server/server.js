cd1398a5e0f28303d97b6cab854bd9ad48dcaa07
vshnu <pandeyvshnu@gmail.com>
2018-09-09 17:40:19 +0530

    Emit ne message

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const express = require('express');
 
-
-var publicPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-  console.log('Server is up ');
-})
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
