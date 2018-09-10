0a6486c876d55483335741a67a8b2eda5f77de58
vshnu <pandeyvshnu@gmail.com>
2018-09-10 19:43:24 +0530

    Html form with chat

--- public/js/index.js
+++ public/js/index.js
@@ -25,4 +25,26 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message){
   console.log('newMessage', message);
+
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#message').append(li);
+});
+
+// socket.emit('createMessage', {
+//  from: 'Frank',
+//  text: 'Hi'
+// }, function (data) {
+// 	console.log('Got it', data);
+// });
+
+jQuery('#message-form').on('submit', function (e) {
+	e.preventDefault();
+
+	socket.emit('createMessage', {
+		from:'User',
+		text: jQuery('[name=message]').val()
+	}, function () {
+	});
 });
--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-
 const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -15,12 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+
  // socket.emit('newEmail', {
  //   from : 'vishnu@gmail.com',
  //   text: 'Hey how are you',
  //   createdAt : 23
  // });
- // 
+ //
  // socket.emit('newMessage', {
  //   from: 'john',
  //   text: 'see you then',
@@ -30,13 +31,40 @@ io.on('connection', (socket) => {
  // socket.on('createEmail', (newEmail) => {
  //   console.log('createEmail', newEmail);
  // });
- socket.on('createMessage', (message) => {
+
+// socket.emit('newMessage', {
+//   from: 'Admin',
+//   text: 'Welcome to chat app',
+//   createdAt: new Date().getTime()
+// });
+
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
+
+
+ // socket.broadcast.emit('newMessage', {
+ //   from: 'Admin',
+ //   text: 'New user joined',
+ //   createdAt: new Date().getTime()
+ // });
+
+socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user join'));
+
+
+ socket.on('createMessage', (message, callback) => {
    console.log('createMessage', message);
-   io.emit('newMessage', {
-     from: message.from,
-     text: message.text,
-     createddAt: new Date().getTime()
-   });
+   io.emit('newMessage', generateMessage(message.from, message.text));
+   callback('This is from server');
+   // {
+   //   from: message.from,
+   //   text: message.text,
+   //   createddAt: new Date().getTime()
+
+   // socket.broadcast.emit('newMessage', {
+   //   from: message.from,
+   //   text: message.text,
+   //   createdAt: new Date().getTime()
+
+  // });
  });
 
   socket.on('disconnect', () => {
